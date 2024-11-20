af9d64d60ee4b6e7415386ea16e9155e47be5b18
AlanYu <squid78313@gmail.com>
2024-11-21 01:18:28 +0800

    anu_spider: 抓取所有(包括換頁)的課程url

--- universities_scrapy/universities_scrapy/spiders/anu_spider.py
+++ universities_scrapy/universities_scrapy/spiders/anu_spider.py
@@ -8,17 +8,21 @@ class AnuSpiderSpider(scrapy.Spider):
     detail_url_list = []
     
     def parse(self, response):
+        # 發送form request
         return scrapy.FormRequest.from_response(
             response,
             formid='views-exposed-form-campaign-course-page-block-5',
             formdata={'combine': 'Bachelor'},
             callback=self.after_search
         )
-        
+    
+    # 搜尋Bachelor後的解析
     def after_search(self, response):
         cards = response.css('.acc-card-body')
         for card in cards:
+            # 抓取課程名稱
             title = card.css('h4 strong::text').get()
+            # 抓取課程網址
             detail_url = card.css('.acc-card-links a:nth-of-type(2)::attr(href)').get()
             self.detail_url_list.append(detail_url)
             print(f'title: {title}, detail_url: {detail_url}')
@@ -26,7 +30,8 @@ class AnuSpiderSpider(scrapy.Spider):
         # 換頁
         next_page = response.css('li.pager__item.pager__item--next a::attr(href)').get()
         if next_page is not None:
-            yield response.follow(next_page, self.parse)
-        
-        print(self.detail_url_list)
+            yield response.follow(next_page, self.after_search)
+    
+    def closed(self, reason):    
+        print(f'共有 {len(self.detail_url_list)} 筆資料\n{self.detail_url_list}')
         
