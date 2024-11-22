4c676069ae80c143da08c632dbff2f14ff0cac7c
Alanyu <squid78313@gmail.com>
2024-11-22 18:06:55 +0800

    sydney_spider.py: urls會有重複的，須排除

--- universities_scrapy/settings.py
+++ universities_scrapy/settings.py
@@ -17,14 +17,15 @@ SPIDER_MODULES = ["universities_scrapy.spiders"]
 NEWSPIDER_MODULE = "universities_scrapy.spiders"
 
 LOG_LEVEL = 'WARNING'  # 或 'ERROR'
+
+# 設置用户代理
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = "universities_scrapy (+http://www.yourdomain.com)"
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = False
 
-# 設置用户代理
-USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
 
 
 # Set settings whose default value is deprecated to a future-proof value
--- universities_scrapy/spiders/sydney_spider.py
+++ universities_scrapy/spiders/sydney_spider.py
@@ -4,8 +4,10 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from universities_scrapy.items import UniversityScrapyItem
+from selenium.common.exceptions import TimeoutException 
 from selenium.common.exceptions import NoSuchElementException
 import re
+import time
 
 class SydneySpiderSpider(scrapy.Spider):
     name = "sydney_spider"
@@ -33,6 +35,7 @@ class SydneySpiderSpider(scrapy.Spider):
                 next_btn = pagination.find_element(By.CSS_SELECTOR, 'button[data-testid="pagination-next-button"]')
                 driver.execute_script("arguments[0].click();", next_btn)
 
+
                 # 等待新頁面加載
                 wait = WebDriverWait(driver, 10)
                 wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'section.m-content.m-course-card')))
@@ -50,17 +53,42 @@ class SydneySpiderSpider(scrapy.Spider):
 
         print(f'共有{len(self.course_urls)}個課程連結')
         
+        for course in self.course_urls:
+            # print(f'{course['name']}\n{course['url']}\n')
+            driver.get(course['url'])
+            print(f'正在爬取課程: {course['name']}\n{course['url']}\n')
+            wait = WebDriverWait(driver, 20)
+            try:
+                wait.until(lambda driver: driver.execute_script('return document.readyState') == 'complete')
+                wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '.m-key-information__list')))
+                course_page = scrapy.Selector(text=driver.page_source)
+                info = course_page.css('.m-key-information__list')
+                if not info:
+                    print(f'注意!!!\n{course['name']}沒有找到.m-key-information__list元素\n{course['url']}\n')
+                    continue
+            
+            # # 抓取學費
+            # fee = info.css('.m-key-information__list__right-fees-info-content-list--price::text').get()
+            # print(fee)
+            except TimeoutException:
+                print(f'{course['name']}頁面加載超時: {course['url']}\n')
+                continue
+            
     
         
     # 提取課程連結
     def extract_course_urls(self, response):
         cards = response.css("section.m-content.m-course-card")
         for card in cards:
+            course_name = card.css('h3.m-title.m-course-card__link-title::text').get()
             course_url = card.css('a.m-link.m-link--default::attr(href)').get()
-            self.course_urls.append(course_url)
+            self.course_urls.append({'name': course_name, 'url': course_url}) 
 
+        
+            
+               
     # 爬取課程頁面
     def parse_course_page(self, response):
-        print(f'正在爬取課程: {response.url}')
+        yield print(f'正在爬取課程: {response.url}')
 
 
