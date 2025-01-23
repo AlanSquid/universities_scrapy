9df360de099001e085269913ae049c290f3d7709
Yazhu <dodo891216@gmail.com>
2025-01-23 12:05:54 +0800

    修改adelaide_spider

--- universities_scrapy/spiders/adelaide_spider.py
+++ universities_scrapy/spiders/adelaide_spider.py
@@ -6,12 +6,12 @@ class AdelaideSpider(scrapy.Spider):
     name = "adelaide_spider"
     allowed_domains = ["www.adelaide.edu.au"]
     base_url = "https://www.adelaide.edu.au"
-    start_urls = ["https://www.adelaide.edu.au/degree-finder?v__s=bachelor&m=view&dsn=program.source_program&adv_avail_comm=1&adv_acad_career=0&adv_degree_type=0&adv_atar=0&year=2025&adv_subject=0&adv_career=0&adv_campus=0&adv_mid_year_entry=0"]
+    start_urls = ["https://www.adelaide.edu.au/degree-finder/?v__s=&m=view&dsn=program.source_program&adv_avail_comm=1&adv_acad_career=0&adv_degree_type=1&adv_atar=0&year=2025&adv_subject=0&adv_career=0&adv_campus=0&adv_mid_year_entry=0","https://www.adelaide.edu.au/degree-finder/?v__s=&m=view&dsn=program.source_program&adv_avail_comm=1&adv_acad_career=0&adv_degree_type=10&adv_atar=0&year=2025&adv_subject=0&adv_career=0&adv_campus=0&adv_mid_year_entry=0"]
     full_link_list=[]
 
     def parse(self, response):
         links = response.css(
-            "div.c-degree-finder__filter-results div.df_list_filter_ug ul.c-degree-finder__filter-results__list li a::attr(href)"
+            "div.c-degree-finder__filter-results  ul.c-degree-finder__filter-results__list li a::attr(href)"
         ).getall()
         
         full_links = [self.base_url + link.strip() for link in links if link]
@@ -20,17 +20,38 @@ class AdelaideSpider(scrapy.Spider):
             yield response.follow(link, self.page_parse)
     
     def page_parse(self, response): 
-        
+        degree_level_id = None
+
         # 取得課程名稱
         course_name = response.css('h1::text').get().strip()
-        
+    
+        if "bachelor" in course_name.lower():
+            degree_level_id = 1
+        elif "master" in course_name.lower(): 
+            degree_level_id = 2
+
+        if course_name.lower().startswith("master of"):
+            course_name = course_name.split("of", 1)[1].strip()
+        elif course_name.lower().startswith("bachelor of"):
+            course_name = course_name.split("of", 1)[1].strip()
+    
         # 取得duration
-        duration = response.css('li.c-icon-box__column') \
+        duration_info = response.css('li.c-icon-box__column') \
                         .xpath('.//h3[contains(text(), "Duration")]/following-sibling::div[@class="c-icon-box__description"]//text()') \
                         .getall()
-        duration = ' '.join([d.strip() for d in duration if d.strip()])
-        duration = re.sub(r'\s+', ' ', duration).strip()
-        
+        if duration_info:
+            duration_info = ' '.join([d.strip() for d in duration_info if d.strip()])
+            duration_info = re.sub(r'\s+', ' ', duration_info).strip()
+            match = re.search(r'\d+(\.\d+)?', duration_info)  # 使用正則表達式查找數字
+            if match:
+                duration = float(match.group())  # 提取匹配內容並轉換為 float
+            else:
+                duration = None  # 如果沒有匹配到數字
+        else:
+            duration_info = None
+            duration = None 
+
+
         # 取得location
         locations = response.css('li.c-icon-box__column') \
         .xpath('.//h3[contains(text(), "Location")]/following-sibling::div[@class="c-icon-box__description"]//a/text() |.//h3[contains(text(), "Location")]/following-sibling::div[@class="c-icon-box__description"]/text()')\
@@ -58,19 +79,24 @@ class AdelaideSpider(scrapy.Spider):
         # 如果有找到分數，處理並建立結果
         if ielts_overall:
             overall_score = re.search(r'(\d+(\.\d+)?)', ielts_overall)
-            english_requirement = f"IELTS {overall_score.group(1)}"
+            eng_req_info = f"IELTS {overall_score.group(1)}"
+            eng_req = overall_score.group(1)
         else:
-            english_requirement = None
+            eng_req_info = None
+            eng_req = None
 
         university = UniversityScrapyItem()
-        university['name'] = 'University of Adelaide'
-        university['ch_name'] = '阿德雷得大學'
-        university['course_name'] = course_name
-        university['min_tuition_fee'] = tuition_fee
-        university['location'] =  locations
-        university['english_requirement'] = english_requirement
+        university['university_id'] = 24
+        university['name'] = course_name
+        university['min_fee'] = tuition_fee
+        university['max_fee'] = tuition_fee
+        university['campus'] =  locations
+        university['eng_req'] = eng_req
+        university['eng_req_info'] = eng_req_info
         university['duration'] = duration
+        university['duration_info'] = duration_info
         university['course_url'] = response.url
+        university['degree_level_id'] = degree_level_id
 
         yield university
     def closed(self, reason):    
