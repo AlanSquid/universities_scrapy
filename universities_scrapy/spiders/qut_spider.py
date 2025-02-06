79c7de98a59f973e7f631a98edde0b7e800a9360
Yazhu <dodo891216@gmail.com>
2025-02-06 15:49:13 +0800

    修改qut_spider

--- universities_scrapy/spiders/qut_spider.py
+++ universities_scrapy/spiders/qut_spider.py
@@ -1,8 +1,6 @@
 import re
 import time
-import json
 import scrapy
-from scrapy_selenium import SeleniumRequest
 from universities_scrapy.items import UniversityScrapyItem
 
 class QutSpiderSpider(scrapy.Spider):
@@ -12,54 +10,45 @@ class QutSpiderSpider(scrapy.Spider):
     seen_urls = set()
     start_time = time.time()
     non_international_num = 0
-
-    def start_requests(self):
-        # 用 response.follow 發送起始請求
-        url = self.start_urls[0]
-        yield SeleniumRequest(url=url, callback=self.parse)
-
+    
     # 到 https://www.qut.edu.au/study/international 頁面找到15個課程大類
     def parse(self, response):
-        urls = response.css('ul.study-area-links li.list-links a.arrow-link::attr(href)').getall()
+        urls = response.css('div#study-areas ul.study-area-links li.list-links a.arrow-link::attr(href)').getall()
         for url in urls:
-            if url:
-                # 跟隨連結進入新的頁面
-                yield response.follow(url, callback=self.parse_areas)
+            # 跟隨連結進入新的頁面
+            yield response.follow(url, callback=self.parse_areas)
 
     # 進入各課程大類共三種狀況 1. 需先選擇undergraduate 或 postgraduate。(e.g. Justice) 2. 有分不同的課程類型。(e.g. Business) 3. 直接有課程
     # 第3種狀況中的又有Create Arts的部分比較特殊，又進一步細分小類別
     def parse_areas(self, response):
         # 檢查是否存在 "Explore our undergraduate courses"
-        undergraduate_button = response.css('a.button.blue.button--blue-outline.arrow.mb-3.study-area-list-buttons.w-100')
-        course_links = response.css('ul.study-area-links li.list-links a.arrow-link')
+        explore_courses_button = response.css('a.button.blue.button--blue-outline.arrow.mb-3.study-area-list-buttons.w-100::attr(href)').getall()
+        course_links = response.css('ul.study-area-links li.list-links a.arrow-link::attr(href)').getall()
         bachelor_courses = response.css('.row .col-lg-4')
         
-        # 處理第一種狀況
-        if undergraduate_button:
-            button_text = undergraduate_button.css('::text').get()
-            if button_text and 'Explore our undergraduate courses' in button_text:
-                url = undergraduate_button.attrib.get('href')
+        # # 處理第一種狀況 ex: https://www.qut.edu.au/study/international/communication
+        if explore_courses_button:
+            for course_button in explore_courses_button:
+                # print(course_button) 確認只有?postgraduate和?undergraduate
                 yield response.follow(
-                    url=response.urljoin(url),
+                    url=response.urljoin(course_button),
                     callback=self.parse_course_link,
                 )
 
-        # 第二種狀況
+        # 第二種狀況 ex: https://www.qut.edu.au/study/international/business
         elif course_links:
             for link in course_links:
-                course_name = link.css('::text').get().strip()
-                url = link.css('::attr(href)').get()
                 yield response.follow(
-                    url=response.urljoin(url),
+                    url=response.urljoin(link),
                     callback=self.parse_course_link,
                 )
 
-        # 第三種狀況
+         # 第三種狀況 ex: https://www.qut.edu.au/study/international/creative-arts
         elif bachelor_courses:
             for course in bachelor_courses:
                 course_name = course.css('h3::text').get(default='').strip()
-                
-                if 'Bachelor' in course_name:
+            
+                if 'Bachelor' in course_name and 'Honours' not in course_name:
                     course_link = course.css('a.arrow-link::attr(href)').get()
                     
                     # 如果是art中的creative art類別，要到parse_creative_art_course再進一層解析
@@ -77,46 +66,54 @@ class QutSpiderSpider(scrapy.Spider):
                         yield response.follow(
                             url=course_link,
                             callback=self.parse_course_page,
-                            meta={'course': course_name, 'url': course_link},
                         )
+
     # 專門處理creative art小類別的url的function
     def parse_creative_art_course(self, response):
         art_course_names = response.css('li.no-list-bullets a::text').getall()
         art_course_links = response.css('li.no-list-bullets a::attr(href)').getall()
         for i in range(len(art_course_names)):
-            if 'Bachelor' in art_course_names[i]:
-                art_course_name = art_course_names[i].strip()
+            if 'Bachelor' in art_course_names[i] or 'Master' in art_course_names[i]:
                 art_course_link = art_course_links[i].strip()
                 if "?international" not in str(art_course_link):
                     art_course_link = response.urljoin(art_course_link) + "?international"
                 yield response.follow(
                     url=art_course_link,
                     callback=self.parse_course_page,
-                    meta={'course': art_course_name, 'url': art_course_link},
                 )
     # 除了creative art小類別，蒐集其他網頁的課程url
     def parse_course_link(self, response):
-        bachelor_courses = response.css('div.col-md-12 a.course-page-link.qut-course-page-link')
-        
-        if bachelor_courses:
-            for course in bachelor_courses:
-                course_name = course.css('::text').get().strip()
-                course_link = course.attrib.get('href')
-                if course_name and course_link and 'Bachelor' in course_name:
-                    if "?international" not in str(course_link):
-                        course_link = response.urljoin(course_link) + "?international"
-                    yield response.follow(
-                        url=course_link,
-                        callback=self.parse_course_page,
-                        meta={'course': course_name, 'url': course_link},
-                    )
-                    
+        # div.course-details 的 id:data-course-audience="DOM,INT" 國際生要有 "INT"
+        # 取得 h4 是 single-degrees 和 Masters 底下的 div.course-list 底下的 div.course-details 底下的 a.course-page-link
+        courses = response.xpath('//h4[contains(text(), "Single degrees") or contains(text(), "Masters")]'
+                                '/following-sibling::div[contains(@class, "course-list")][1]'
+                                '//div[contains(@class, "course-details") and contains(@data-course-audience, "INT")]'
+                                '//a[contains(@class, "course-page-link")]/@href').getall()        
+        if courses:
+            for course in courses:   
+                if not course or "honours" in course:
+                    # print('honours跳過:',course)
+                    continue
+                if "?international" not in str(course):
+                    international_course = response.urljoin(course) + "?international"
+                yield response.follow(
+                    url = international_course,
+                    callback = self.parse_course_page,
+                )
+                
     # 在課程網頁中抓取資料
     def parse_course_page(self, response):
-        meta_data = response.meta 
-        course = meta_data['course']
-        url = meta_data['url']
-        
+        course_name = response.css('h1.hero__header__title span::text').get()
+        keywords = ["Bachelor of", "Master of", "Doctor of"]
+        if not course_name or sum(course_name.count(keyword) for keyword in keywords) >= 2:
+            return 
+
+        degree_level_id = None
+        if "Bachelor of" in course_name:
+            degree_level_id = 1
+        elif "Master of" in course_name: 
+            degree_level_id = 2
+    
         # 費用處理
         tuition_fee = ''
         fees_all = response.css('div.box-wrap.col-sm-6 div.box-content p::text').getall()
@@ -129,34 +126,47 @@ class QutSpiderSpider(scrapy.Spider):
                     tuition_fee = match.group(1).replace(',', '')  # 去掉千位分隔符
                     break
         
-        # 校區處理
+        # # 校區處理
         locations = response.css('ul[data-course-map-key="quickBoxDeliveryINT"] li::text').getall()
         location = ', '.join(locations)
-        
-        # 學習時間處理
+        if location == "":
+            location = None
+
+        # # 學習時間處理
         durations = response.css('li[data-course-map-key="quickBoxDurationINTFt"]::text').getall()
-        duration = ', '.join([d.strip() for d in durations])
-        
-        # 英文門檻
-        english_requirement = ''
-        english_requirements = response.css('table#int-elt-table td#elt-overall::text').getall()
-        for number in english_requirements:
+        duration_info = ', '.join([d.strip() for d in durations])
+        if duration_info:
+            duration_info = duration_info.strip()
+            match = re.search(r'\d+(\.\d+)?', duration_info)  # 使用正則表達式查找數字
+            if match:
+                duration = float(match.group())  # 提取匹配內容並轉換為 float
+            else:
+                duration = None  # 如果沒有匹配到數字
+
+        # # 英文門檻
+        eng_req = ''
+        eng_reqs = response.css('table#int-elt-table td#elt-overall::text').getall()
+        for number in eng_reqs:
             if type(number) == str:
-                english_requirement = number.strip()
+                eng_req = number.strip()
                 break
         
-        if tuition_fee and location and english_requirement:
+        if tuition_fee and eng_req:
             self.seen_urls.add(response.url)
             university = UniversityScrapyItem()
-            university['name'] = "Queensland University of Technology"
-            university['ch_name'] = "昆士蘭科技大學"
-            university['course_name'] = course
-            university['min_tuition_fee'] = tuition_fee
-            university['english_requirement'] = f'IELTS (Academic) {english_requirement}'
-            university['location'] = location
-            university['course_url'] = url
+            university['university_id'] = 20
+            university['name'] = course_name
+            university['min_fee'] = tuition_fee
+            university['max_fee'] = tuition_fee
+            university['eng_req'] = eng_req
+            university['eng_req_info'] = f'IELTS (Academic) {eng_req}'
+            university['campus'] = location
             university['duration'] = duration
+            university['duration_info'] = duration_info
+            university['degree_level_id'] = degree_level_id
+            university['course_url'] = response.url
             yield university
+
         else:
             audience_element = response.xpath('//span[@data-course-audience="DOM" and text()="This course is only available for Australian and New Zealand students."]')
             if audience_element:
