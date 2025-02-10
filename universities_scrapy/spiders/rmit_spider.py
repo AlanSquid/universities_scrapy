31c42f6c3b8358d217cb33bb64f293fa6628f11d
Yazhu <dodo891216@gmail.com>
2025-02-10 14:16:42 +0800

    修改rmit_spider

--- universities_scrapy/spiders/rmit_spider.py
+++ universities_scrapy/spiders/rmit_spider.py
@@ -1,5 +1,4 @@
 import re
-import time
 import scrapy
 from scrapy import Selector
 from scrapy_selenium import SeleniumRequest
@@ -8,162 +7,91 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from universities_scrapy.items import UniversityScrapyItem
 
-
 class RmitSpiderSpider(scrapy.Spider):
     name = "rmit_spider"
     allowed_domains = ["www.rmit.edu.au"]
-    start_urls = ["https://www.rmit.edu.au/study-with-us/international-students/programs-for-international-students/courses-for-international-students-by-study-area?activeTab=All"]
+    # start_urls = ["https://www.rmit.edu.au/study-with-us/international-students/programs-for-international-students/courses-for-international-students-by-study-area?activeTab=All"]
+    start_urls = ["https://www.rmit.edu.au/content/rmit/au/en/study-with-us/international-students/programs-for-international-students/courses-for-international-students-by-study-area/jcr:content/body-gridcontent/tabs/all/rmitprogramlist_copy.model.json?t=1739160669494&data=yes&facetFilter=rmit:study_type/undergraduate_degree","https://www.rmit.edu.au/content/rmit/au/en/study-with-us/international-students/programs-for-international-students/courses-for-international-students-by-study-area/jcr:content/body-gridcontent/tabs/all/rmitprogramlist_copy.model.json?t=1739161591376&data=yes&facetFilter=rmit:study_type/postgraduate_study"]
     all_course_url = []
-    all_course_name = []
-    start_time = time.time()
-    retry_quota = 40
     
-    # custom_settings = {
-    #     'FEEDS': {
-    #         'universities_output/rmit_data.csv': {
-    #             'format': 'csv',
-    #             'encoding': 'utf-8-sig',
-    #         }
-    #     },
-    #     'FEED_EXPORT_FIELDS': [
-    #         'name', 'ch_name', 'course_name', 'min_tuition_fee',
-    #         'english_requirement', 'location', 'course_url', 'duration'
-    #     ],
-    # }
-    
-    def start_requests(self):
-        url = self.start_urls[0]
-        yield scrapy.Request(
-            url,
-            meta=dict(
-                playwright=True,
-                playwright_include_page = True,
-            ),
-            callback=self.parse_all_course, 
-        )
+    def parse(self, response):
+        data = response.json()  
+        for item in data['programs']['programs']:
+            course_url = item['programUrl']
+            course_name = item['programName']
+            # 跳過雙學位, Honours, Online, Graduate Certificate, Diploma
+            keywords = ["bachelor-degrees", "masters-by-coursework" ,"Bachelor of" , "Master of", "major"]
+            if not course_url  or sum(course_name.count(keyword) for keyword in keywords) >= 2 or sum(course_url.count(keyword) for keyword in keywords) < 1:
+                # print('跳過:',course_name)
+                continue
+            self.all_course_url.append(course_url)
+
+            yield response.follow(course_url, self.page_parse)
+   
+    def page_parse(self, response):
+        course_name = response.css("div.header-gridcontent h1::text").get()
+        # 去除前後空白字符
+        if course_name:
+            course_name = course_name.strip()
+            
+        if "bachelor" in response.url:
+            degree_level_id = 1
+        elif "master" in response.url: 
+            degree_level_id = 2
+
+        # 費用
+        fee_info = response.css('div.quickfacts div.b-international dd.qf-int-fee p::text').get()
+        tuition_fee = None
+        if fee_info and "AU$" in fee_info:
+            fee = fee_info.strip()
+            # 使用正則表達式匹配 "AU$" 後的數字
+            pattern = r"AU\$\s*(\d[\d,]*)"  # 匹配 AU$ 後的數字，允許逗號
+            match = re.search(pattern, fee)
+            if match:
+                tuition_fee = match.group(1).replace(",", "")  # 去掉逗號
         
-    async def parse_all_course(self, response):
-        page = response.meta["playwright_page"]
-        await page.wait_for_selector("li[role='presentation'] a.program-list-facet[data_type='rmit:study_type/undergraduate_degree']", timeout=30 * 1000)
-        page_content = scrapy.Selector(text=await page.content())
-        all_course_div = page_content.css('div.rmitprogramlist.aem-GridColumn.aem-GridColumn--default--12 tbody tr.lcl.intnl td.pl--programname.hidelink a')
-        await page.close()
-        
-        for course in all_course_div:
-            course_name = course.css('::text').get()
-            course_url = course.css('::attr(href)').get().strip()
-            course_url = response.urljoin(course_url)
-            major = ""
-            is_major = False
-            
-            if 'Bachelor' in course_name:
-                # 某些科系會進一步細分類群，但主要課程資訊(費用、門檻等)一樣需要去主要科系查看
-                if course_url.count('/') == 8:
-                    # 使用 rsplit 去掉 URL 的最後一部分
-                    course_url, major = course_url.rsplit('/', 1)  # 這裡會將 URL 的最後部分賦值給 major
-                    is_major = True
-                yield SeleniumRequest(
-                    url=course_url,
-                    dont_filter=True,
-                    meta=dict(
-                        course_name=course_name,
-                        is_major=is_major,
-                        major=major,
-                    ), 
-                    callback=self.parse_course, 
-                )
-                
-    def parse_course(self, response):
-        driver = response.meta["driver"]
-        driver.get(response.url)
-        
-        try:
-            course_name = response.meta['course_name']
-            is_major = response.meta['is_major']
-            major = response.meta['major']
-            url = response.url
-            
-            wait = WebDriverWait(driver, 60)
-            wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "span.cbResultSetData.cbResultSetNestedAlign")))
-            
-            page_source = driver.page_source
-            selector = Selector(text=page_source)
-                    
-            # 費用
-            fee_span = selector.css('span.cbResultSetData.cbResultSetNestedAlign::text').getall()
-            for span in fee_span:
-                if "AU$" in span:
-                    fee = span.strip()
-                    # 使用正則表達式匹配 "AU$" 後的數字
-                    pattern = r"AU\$\s*(\d[\d,]*)"  # 匹配 AU$ 後的數字，允許逗號
-                    match = re.search(pattern, fee)
-                    if match:
-                        tuition_fee = match.group(1).replace(",", "")  # 去掉逗號
-                    break
-            
-            # 英文門檻
-            eng_req_li = selector.css('div.cmp-text li::text').getall()
-            for li_text in eng_req_li:
-                if 'IELTS' in li_text:
-                    pattern = r"IELTS\s*\((.*?)\)\s*[:：]\s*.*?(\d+(\.\d+)?)"
-                    match = re.search(pattern, li_text)
-                    if match:
-                        english_requirement = match.group(2)  # 提取數字部分
-                    
-            # 校區
-            locations = selector.xpath('//dt[text()=" Location:"]/following-sibling::dd[@class="desc qf-int-location"]/text()').getall()
-            locations = [all_location.strip() for all_location in locations]
-            location = ', '.join(locations)
-            
-            # 學制(期間)
-            durations = selector.xpath('//dt[text()=" Duration:"]/following-sibling::dd[@class="desc qf-int-duration"]/text()').getall()
-            durations = [all_duration.strip() for all_duration in durations]
-            duration = ', '.join(durations)
-            
-            # 將有分major的課程
-            if is_major:
-                url = f"{response.url}/{major}"
-            else:
-                url = response.url
-            
-            if url not in self.all_course_url:
-                if course_name not in self.all_course_name:
-                    self.all_course_url.append(url)
-                    self.all_course_name.append(course_name)
-            else:
-                return
-            
-            university = UniversityScrapyItem()
-            university['name'] = "Royal Melbourne Institute of Technology"
-            university['ch_name'] = "墨爾本皇家理工大學"
-            university['course_name'] = course_name
-            university['min_tuition_fee'] = tuition_fee
-            university['english_requirement'] = f'IELTS (Academic) {english_requirement}'
-            university['location'] = location
-            university['course_url'] = url
-            university['duration'] = duration
-            yield university
-        
-        except Exception as e:
-            # logging.exception(f"錯誤發生在 URL: {response.url}")
-            # print(f"錯誤訊息: {str(e)}")
-            if self.retry_quota == 0:
-                print(f'失敗的網址: {url}')
-                return
-            print(f'剩餘可重試次數: {self.retry_quota}')
-            self.retry_quota -= 1
-            time.sleep(1)
-            yield SeleniumRequest(
-                url=response.url,
-                dont_filter=True,
-                meta=dict(
-                    course_name=course_name,
-                    is_major=is_major,
-                    major=major,
-                ), 
-                callback=self.parse_course, 
-            )
-        
+        # 英文門檻
+        eng_req = eng_req_info = None
+        eng_req_li = response.css('div.intl-par.responsivegrid.international div#english-language-requirments-experiencefragment li::text').getall()
+        for li_text in eng_req_li:
+            if 'IELTS' in li_text:
+                eng_req_info = li_text
+                pattern = r"IELTS\s*\((.*?)\)\s*[:：]\s*.*?(\d+(\.\d+)?)"
+                match = re.search(pattern, li_text)
+                if match:
+                    eng_req = match.group(2)  # 提取數字部分
+
+        # 校區
+        locations = response.xpath('//dt[text()=" Location:"]/following-sibling::dd[@class="desc qf-int-location"]/text()').getall()
+        locations = [all_location.strip() for all_location in locations]
+        location = ', '.join(locations)
+
+        # 學制(期間)
+        durations = response.xpath('//dt[text()=" Duration:"]/following-sibling::dd[@class="desc qf-int-duration"]/text()').getall()
+        durations = [all_duration.strip() for all_duration in durations]
+        duration_info = ', '.join(durations)
+        pattern = r"(\d+(\.\d+)?)"
+        match = re.search(pattern, duration_info)
+        if match:
+            first_number = match.group(1)
+            duration=first_number
+        else:
+            duration=None
+
+        university = UniversityScrapyItem()
+        university['university_id'] = 32
+        university['name'] = course_name
+        university['min_fee'] = tuition_fee
+        university['max_fee'] = tuition_fee            
+        university['eng_req'] = eng_req       
+        university['eng_req_info'] = eng_req_info
+        university['campus'] = location
+        university['duration'] = duration
+        university['duration_info'] = duration_info
+        university['degree_level_id'] = degree_level_id        
+        university['course_url'] = response.url
+        yield university
+   
     def close(self):
         print(f"墨爾本皇家理工大學({self.name})總共{len(self.all_course_url)}個科系")
         # end_time = time.time()
